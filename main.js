61d2c2873c4552f0e5c8fea0c3b856e2d05ce9f5
T-boy96 <szu.tomi@gmail.com>
2026-01-17 11:49:30 +0100

    cleaned up code, renaming

--- main.js
+++ main.js
@@ -1,44 +1,42 @@
-const menubuttons = document.querySelectorAll('.menu button');
-const sections = document.querySelectorAll('.content section');
+const mediaQueryTablet = window.matchMedia('(min-width: 768px) and (max-width: 1023px)');
+const mediaQueryDesktop = window.matchMedia('(min-width: 1024px)');
+
+const hamburger = document.querySelector('.hamburger');
+const menuButtons = document.querySelectorAll('.menu button');
+const triggerTds = document.querySelectorAll('.trigger-td')
+const contentSections = document.querySelectorAll('.content section');
 const dates = document.querySelectorAll('#dates a');
 const contactLinks = document.querySelectorAll('#contact a');
 
-const menuButtonMobile = document.querySelector('.menu-button');
-
-menuButtonMobile.addEventListener('click', () => {
-    menuButtonMobile.classList.toggle('active');
-    const expanded = menuButtonMobile.getAttribute('aria-expanded') === 'true';
-    menuButtonMobile.setAttribute('aria-expanded', String(!expanded));
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+    hamburger.setAttribute('aria-expanded', String(!expanded));
 });
 
-
-menubuttons.forEach(mb => {
+menuButtons.forEach(mb => {
     mb.addEventListener('click', () => {
         const targetId = mb.dataset.target;
-        sections.forEach(section => {
+        contentSections.forEach(section => {
             section.hidden = section.id !== targetId;
         });
-        menubuttons.forEach(b => {
+        menuButtons.forEach(b => {
             (b.dataset.target === targetId) ? b.classList.add('active') : b.classList.remove('active');
         });
     });
 });
 
-const mediaQuery = window.matchMedia('(min-width: 1024px)');
 
-const triggertds = document.querySelectorAll('.trigger-td')
-triggertds.forEach(td => {
-    if (mediaQuery.matches) {
+
+triggerTds.forEach(td => {
+    const cell = td.nextElementSibling;
+    if (mediaQueryDesktop.matches) {
         td.addEventListener('mouseenter', function () {
-            const cell = this.nextElementSibling;
-            const secondCell = cell.nextElementSibling;
-            cell.setAttribute('hidden', '');
-            secondCell.classList.add('is-visible');
+            td.setAttribute('hidden', '');
+            cell.classList.add('is-visible');
         }, { once: true });
-
     } else {
-        const cell = td.nextElementSibling;
-        cell.setAttribute('hidden', '');
+        td.setAttribute('hidden', '');
     }
 });
 
@@ -50,10 +48,6 @@ function clickHandler(e) {
     });
 }
 
-dates.forEach(d => {
-    d.addEventListener('click', clickHandler);
+[dates, contactLinks].forEach(link => {
+    link.addEventListener('click', clickHandler);
 });
-
-contactLinks.forEach(cl => {
-    cl.addEventListener('click', clickHandler);
-})
